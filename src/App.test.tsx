def9414f3e3d8849a81bdfbe8db577f320aee6c2
Hachoorry <99401872+Hachoorry@users.noreply.github.com>
2022-03-24 20:17:01 +0800

    change app.test.tsx

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders Learn fail Reactcococococcol link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/Learn fail Reactcococococcol/i);
   expect(linkElement).toBeInTheDocument();
 });
